fb488d73d9f1dd87ea750e93b9fdca7171a429d1
Jose Campos <Jose_cmps14@yahoo.com>
2022-11-17 20:55:33 -0600

    commit tree a872b6c502961bdfb7215c8db3c8e9e3839af0f0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers,string) {
+    return drivers.filter((user)=> user.toLowerCase()===string.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers,string) {
+    return drivers.filter((firstLetter)=> firstLetter[0]===string[0])
+}
+
+
+
+function matchName(drivers,string) {
+return drivers.filter((item) => item.name === string)
+}
